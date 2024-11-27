f61c577344b78e10304d263a810df351a4d4d797
today072 <databingo@foxmail.com>
2024-11-27 22:45:24 +0800

    update

--- run_rv_freebsd.sh
+++ run_rv_freebsd.sh
@@ -6,7 +6,9 @@
 # 14 pkg have some unoffical support 
 # so 14.1 release
 
-#ssh -p3322 root@127.0.0.1
+# ssh -p3322 root@127.0.0.1
+# scp -P 3322 root@127.0.0.1:/file .
+# scp -P 3322 file root@127.0.0.1:/usr/local/projects/
 qemu-system-riscv64 \
   -machine virt \
   -m 2048M \
@@ -15,10 +17,17 @@ qemu-system-riscv64 \
   -kernel u-boot.bin \
   -drive file=FreeBSD-14.1-RELEASE-riscv-riscv64-zfs.qcow2,format=qcow2,id=hd0 \
   -device virtio-blk-pci,drive=hd0 \
-  -netdev user,id=net0,ipv6=off,hostfwd=tcp::3322-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7890-:7890 \
+  -netdev user,id=net0,ipv6=off,hostfwd=tcp::3322-:22,hostfwd=tcp::7777-:7777,guestfwd=tcp:10.0.2.100:7890-tcp:127.0.0.1:7890 \
   -device virtio-net-device,netdev=net0 \
   -nographic \
  #-monitor none &
+
+# ssh
+# /etc/rc.conf sshd_enable="YES"
+# /etc/ssh/sshd_config PermitRootLogin yes
+# /etc/rc.d/sshd onestart
+# passwd
+# scp -P 3322 init_bsd.sh root@127.0.0.1:/root
 
 # pkg for riscv64
 # config
@@ -30,7 +39,7 @@ qemu-system-riscv64 \
 #  enabled: yes
 # }
 # create FreeBSD.conf and enable: no
-
+# or
 # static
 # fetch http://lonesome.com/FreeBSD/FreeBSD:13:riscv64/latest/Latest/pkg.txz
 # tar xf ./pkg.txz -s ",/.*/,,g" "*/pkg-static"
@@ -39,14 +48,6 @@ qemu-system-riscv64 \
 # or add in ~/.shrc and run . ~/.shrc :
 # alias pkg='/usr/local/projects/bin/pkg-static -o ABI=FreeBSD:13:riscv64'
 
-# ssh
-# /etc/rc.conf sshd_enable=YES
-# /etc/ssh/sshd_config PermitRootLogin yes
-# /etc/rc.d/sshd onestart
-# su -
-# passwd
-# ssh -p2222 root@127.0.0.1
-# scp -P 5555 root@127.0.0.1:/file .
 
 # bigger
 # qemu-img resize *.qcow2 +19G
